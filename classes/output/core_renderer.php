7df7c465f3ecd85dfd6583123c27f4f9856bccb3
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2019-07-09 16:45:10 +0300

    Turn editing on button

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of the classic theme for Moodle
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -14,20 +14,28 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace theme_classic\output;
+/**
+ * Renderers to align Moodle's HTML with that expected by Bootstrap
+ *
+ * @package    theme_dsv_classic
+ * @copyright  2018 Bas Brands
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-use \theme_boost\output\core_renderer as boost_core_renderer;
+namespace theme_dsv_classic\output;
 
 defined('MOODLE_INTERNAL') || die;
 
 /**
  * Renderers to align Moodle's HTML with that expected by Bootstrap
  *
- * @package    theme_classic
+ * Note: This class is required to avoid inheriting Boost's core_renderer,
+ *       which removes the edit button required by Classic.
+ *
+ * @package    theme_dsv_classic
  * @copyright  2018 Bas Brands
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-class core_renderer extends boost_core_renderer {
+class core_renderer extends \core_renderer {
 
 }
